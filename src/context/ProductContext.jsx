dba40ccc40e1621093a72349f30d410fa7ade3de
Antonini333 <pabloantoninivazquez@gmail.com>
2023-02-08 18:21:47 +0100

    Refactor Product Context

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -8,19 +8,18 @@ const ProductContext = createContext({
     setCartItems: () => {},
 });
 
-
-
 const ProductProvider = ({ children }) => {
     const [productList, setProductList] = useState([]);
     const [cartItems, setCartItems] = useState(0);
     const [lastUpdate, setLastUpdate] = useState(null);
     const msHour = 60 * 60 * 1000;
+    const hourHasPassed = () => Date.now() - lastUpdate > msHour;
 
     useEffect(() => {
-        if (!lastUpdate || Date.now() - lastUpdate > msHour) {
+        if (hourHasPassed()) {
             getProductsList()
-                .then(res => {
-                    setProductList(res.data);
+                .then(({data}) => {
+                    setProductList(data);
                     console.log("New update", Date.now())
                     setLastUpdate(Date.now())
                 })
@@ -29,9 +28,7 @@ const ProductProvider = ({ children }) => {
     }, [lastUpdate]);
 
     useEffect(() => {
-        const intervalId = setInterval(() => {
-            setLastUpdate(null);
-        }, msHour);
+        const intervalId = setInterval(() => setLastUpdate(null), msHour);
         return () => clearInterval(intervalId);
     }, []);
 
