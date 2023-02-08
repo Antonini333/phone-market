6383493e70c4e72e773603ce8fbcb2d8fc96864b
Antonini333 <pabloantoninivazquez@gmail.com>
2023-02-08 16:08:29 +0100

    Link header to homepage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProductDetailsPage from "./views/ProductDetailsPage";
 import ProductListPage from "./views/ProductListPage";
+import { Header } from "./components/Header/Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<ProductListPage />} />
           <Route path="/products/:id" element={<ProductDetailsPage />} />
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+export const Header = () => {
+
+    return (
+        <nav>
+            <Link to="/">
+                <h2>Header</h2>
+            </Link>
+        </nav>
+    )
+}
+
