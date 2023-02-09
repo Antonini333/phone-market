340b775e852d52786bd86a8c36a1cac23585f369
Antonini333 <pabloantoninivazquez@gmail.com>
2023-02-09 12:42:32 +0100

    Create useDebounceValue hook for searchbar

---
+++ src/hooks/useDebounceValue.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+const useDebounceValue = (value, time = 250) => {
+    const [debounceValue, setDebounceValue] = useState(value);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDebounceValue(value);
+        }, time);
+
+        return () => clearTimeout(timeout);
+
+    }, [value, time]);
+
+    return debounceValue;
+}
