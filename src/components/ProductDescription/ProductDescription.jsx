7174c494eaab93f506461c045e6f50882f50e66a
Antonini333 <pabloantoninivazquez@gmail.com>
2023-02-13 01:25:34 +0100

    Finish styles on details page and components

--- src/components/ProductDescription/ProductDescription.jsx
+++ src/components/ProductDescription/ProductDescription.jsx
@@ -31,11 +31,13 @@ const ProductDescription = ({ item, loading }) => {
                 <div>loading</div>
             ) : (
                 <div className="c-product-description">
+                    <div className="c-product-description__header">
                     <h3>Description</h3>
+                    </div>
                     <ul className="c-product-description__list">
                         {descriptionArray && descriptionArray.map(({ key, value }) => (
                             <li key={key}>
-                                {capitalizeWords(key)}: {replaceEmptyString(value)}
+                                {<b>{capitalizeWords(key)}</b>}: {replaceEmptyString(value)}
                             </li>
                         ))}
                     </ul>
