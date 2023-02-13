e4629aa8669fe2aedeee1e2834d9d587d9c253a7
Antonini333 <pabloantoninivazquez@gmail.com>
2023-02-13 13:46:19 +0100

    Finish styling product cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,25 @@
-import "../../styles/components/card.scss"
+import React from "react";
+import "../../styles/components/card.scss";
 
-export const Card = ({item}) => {
+export const Card = ({ item }) => {
+
+    const hasPrice = (item) => {
+        if (item.price) return `${item.price} $`
+        return "Not available"
+    }
 
     return (
         <div>
             <div className="c-card">
-                <div className="c-card__image">
-                    <img src={item?.imgUrl} alt="product"/>
-                </div>
                 <div className="c-card__model">
                     {item.brand} {item?.model}
                 </div>
+                <div className="c-card__image">
+                    <img src={item?.imgUrl} loading="lazy" alt="product" />
+                </div>
+
                 <div className="c-card__price">
-                    {item?.price}
+                    <b> {hasPrice(item)}</b>
                 </div>
 
             </div>
