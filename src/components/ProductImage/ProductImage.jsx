a031d5b0b9aadc96fcbf7caa26ed585a70de82c8
Antonini333 <pabloantoninivazquez@gmail.com>
2023-02-13 15:32:13 +0100

    Add back button to product image

--- src/components/ProductImage/ProductImage.jsx
+++ src/components/ProductImage/ProductImage.jsx
@@ -1,9 +1,15 @@
 import "../../styles/components/product-image.scss";
+import {IoMdArrowRoundBack} from "react-icons/io"
+import { useNavigate } from "react-router";
 
 const ProductImage = ({ item }) => {
+const navigate = useNavigate();
 
     return (
         <div className="c-product-image">
+            <button className="c-product-image__back" onClick={() => navigate("/")}>
+                <IoMdArrowRoundBack size={32} color={"white"}/>
+            </button>
             <img src={item?.imgUrl} alt="Product" />
         </div>
     )
